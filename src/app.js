cedc30ab59bfda2cf6a6958c3d8722abadcc5c68
Abdulaleem Mughal <mughalabdulaleem8@gmail.com>
2025-02-28 11:20:59 -0900

    exploring the get, post and delete method

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
-const express = require('express');  
+const express = require('express');
+
 const app = express();
 
-app.get("/", (req, res) => {  
-    res.send("Hello from the dashboard");  
-});  
 
-app.get("/test", (req, res) => {  
-    res.send("Hello from the test file");  
-});  
+app.use("/test", (req, res) => {
+    res.send("Hello from the test file");
+});
 
-app.get("/hello", (req, res) => {  
-    res.send("Hello from the hello file");  
-});  
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Abdul Aleem", lastName: "Mughal"});
+    res.send("Getting the data successfully")
+});
 
-app.listen(7777, () => {  
-    console.log("Server is running on port 7777");  
+app.post("/user", (req, res) => {
+    res.send("Data saved to the database successfully");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("Data deleted successfully");
+});
+
+app.listen(7777, () => {
+    console.log("Server is running on port 7777");
 });
