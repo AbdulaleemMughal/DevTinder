eb4a81e8b2ac737569a8c515dc74a1044c38f729
Abdulaleem Mughal <mughalabdulaleem8@gmail.com>
2025-03-12 11:19:56 -0900

    Diving deep into the API and validation into the schema

--- src/app.js
+++ src/app.js
@@ -4,20 +4,75 @@ const User = require("./models/user");
 
 const app = express();
 
+app.use(express.json()); // to parse json request
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Virat",
-    lastName: "Kohli",
-    emailId: "viratkohli@gmail.com",
-    password: "qaK30MkDaJBeJxgn",
-  });
+  // console.log(req.body);  res.body contains all the data that is written in the body sessiojn of the postman
+
+  const user = new User(req.body);
 
   try {
     await user.save(); // Save user to MongoDB
-    res.send("Virat Kohli registered successfully");
+    res.send("User registered successfully");
   } catch (err) {
     // handling error if occur
     res.status(400).send("Error saving the User" + err.message);
+  }
+});
+
+// Api for fetching the one user profile
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.findOne({ emailId: userEmail }); // find the one user
+    res.send(user);
+  } catch (err) {
+    res.status(500).send("Something went wrong" + err.message);
+  }
+});
+
+// Api for fetching the the arrayof user profile
+
+app.get("/feed", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(500).send("Something went wrong" + err.message);
+  }
+});
+
+// deleteing a user from the databse
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    await User.findByIdAndDelete({ _id: userId });
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(500).send("Something went wrong" + err.message);
+  }
+});
+
+// updating a user from the databse
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "before",
+      runValidators: true // it help us to run the validation on updating any user data
+    });
+    res.send("User Updated Successfully")
+  } catch (err) {
+    res.status(500).send("Something went wrong" + err.message);
   }
 });
 
@@ -29,5 +84,5 @@ connectDataBase()
     });
   })
   .catch((err) => {
-    console.log("Error while Connecting the Database");
+    console.log("Error while Connecting the Database", err.message);
   });
--- src/models/user.js
+++ src/models/user.js
@@ -2,19 +2,45 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        required: true,// the email should not be empty
+        unique: true, // the emailId should be unique
+        lowercase: true, // convert the email into lowercase
+        trim: true // remove the white spaces from the email only from start and the last
     },
     password: {
-        type: String
+        type: String,
+        required: true
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18  // define the minimum age of the user
+    },
+    gender: {
+        type: String,
+        validate(value) {
+            if (!['male', 'female', 'other'].includes(value)) {
+                throw new Error("Gender should be either 'male', 'female' or 'other'");
+            }
+        }
+    },
+    photo: {
+        type: String,
+        default: "https://pinnacle.works/wp-content/uploads/2022/06/dummy-image.jpg" // it store the default value for the attribute
+    },
+    about: {
+        type: String,
+        default: "This is the default about of the user..."
+    },
+    skills: {
+        type: [String]
     }
 });
 
